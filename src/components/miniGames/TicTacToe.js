b401ee6957be30ee5c96fbd4ccfc4cbed2a402d2
spear of codes <bashar.shenineh.com>
2022-03-24 12:48:52 +0100

    Added tictactoe

--- src/components/miniGames/TicTacToe.js
+++ src/components/miniGames/TicTacToe.js
@@ -5,6 +5,18 @@ function TicTacToe(){
     const [turn, setTurn] = useState('x');
     const [cells, setCells] = useState(Array(9).fill(''));
     const [winner, setWinner] = useState();
+    const [clicked , setClicked] = useState(false);
+
+
+    // 1 =  Player 1, 2 = Plaer2, S = Server
+    // 1 Connected && save me=x -> S(x) ; S -> 2Connected (x) && me=o
+    // 2 if(serverSaysX){I am player o} || if serverSaysNothing -> S(x)
+
+    // 1 clicks -> S(click[x], turn) ; S-> 2(click[x])
+
+    // S(turn)->1/2 if(turn==x){turn(o)}else if(turn==o){turn(x)}
+
+    // Save myPlayer (x/o)
 
     const checkForWinner = (squares) => {
         let combos = {
@@ -43,30 +55,37 @@ function TicTacToe(){
     };
 
     const handleClick = (num) => {
-        if (cells[num] !== '') {
-            alert('already clicked');
-            return;
-        }
-
-        let squares = [...cells];
-
-        if (turn === 'x') {
-            squares[num] = 'x';
-            setTurn('o');
-        } else {
-            squares[num] = 'o';
-            setTurn('x');
-        }
-
-        checkForWinner(squares);
-        setCells(squares);
+        // if(turn == me){
+            if (cells[num] !== '') {
+                setClicked(true);
+                setTimeout(() => {
+                    setClicked(false)
+                }, 2000);
+                return;
+            }
+    
+            let squares = [...cells];
+    
+            if (turn === 'x') {
+                squares[num] = 'x';
+                setTurn('o');
+            } else {
+                squares[num] = 'o';
+                setTurn('x');
+            }
+    
+            checkForWinner(squares);
+            setCells(squares);
+        // }else{
+        //     console.log("Not your turn")
+        // }
     };
 
     const handleRestart = () => {
         setWinner(null);
         setCells(Array(9).fill(''));
     };
-
+   
     const Cell = ({ num }) => {
         return <td onClick={() => handleClick(num)}>{cells[num]}</td>;
     };
@@ -87,13 +106,18 @@ function TicTacToe(){
                         <Cell num={5} />
                     </tr>
                     <tr>
+                        <Cell num={8} />
                         <Cell num={6} />
                         <Cell num={7} />
-                        <Cell num={8} />
                     </tr>
                 </tbody>
             </table>
+            {winner &&
             <p>{winner} is the winner!</p>
+            }
+            {
+                clicked && <p>allReadyClicked</p>
+            }
             <button onClick={() => handleRestart()}>Play Again</button>
         </div>
     )
