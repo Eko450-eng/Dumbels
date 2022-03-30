2342a20530a0e5c9258fefc37f844ee0a2c64a15
Eko450-eng <ekrem@wipdesig.de>
2022-03-30 10:09:02 +0200

    Smoll fix

--- src/components/firebase/firebaseConfig.js
+++ src/components/firebase/firebaseConfig.js
@@ -2,16 +2,15 @@ import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseApp = initializeApp({
-  apiKey: "AIzaSyBMTaOBCVzpxqlXgcXSZeDHAoFxXYBHQk8",
-  authDomain: "dcifinalproject.firebaseapp.com",
-  databaseURL: "https://dcifinalproject-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "dcifinalproject",
-  storageBucket: "dcifinalproject.appspot.com",
-  messagingSenderId: "103514347185",
-  appId: "1:103514347185:web:5ec45c548f56185d51490"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 });
 
 const db = getFirestore(firebaseApp)
-
 
 export default db
