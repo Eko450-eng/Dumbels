869613a8e77dde73ca58042b1ed2bf3f69cb8052
Eko450-eng <ekrem@wipdesig.de>
2022-04-05 12:14:58 +0200

    Added chat display and send Chat

--- src/components/Chatscreen.js
+++ src/components/Chatscreen.js
@@ -1,10 +1,35 @@
+import { useEffect, useState } from 'react';
+import { query, collection, onSnapshot } from 'firebase/firestore'
+import db from './firebase/firebaseConfig'
 import Chatinput from './chatComponents/Chatinput';
-import MessageBox from './chatComponents/MessageBox';
+import { v4 as uuid } from 'uuid'
 
 function Chatscreen(){
-	return <div className="Chatscreen">
-			 <MessageBox />
-             <Chatinput />
-		   </div>
+
+    const [ messages, setMessages ] = useState([])
+
+    const getMessages = async() =>{
+        const q = query(collection(db, 'mainChat'))
+        const unsubscribe = onSnapshot(q, (qs) => {
+        setMessages([])
+            qs.forEach((doc)=>{
+                setMessages(old=>[...old, doc.data()])
+            })
+        })
+    }
+
+    useEffect(()=>{
+        getMessages()
+    },[])
+
+    return <div className="Chatscreen">
+            {
+                messages.map(m=>{
+                    console.log(m)
+                    return <p key={uuid()}>{m.message}</p>
+                })
+            }
+            <Chatinput />
+        </div>
 }
 export default Chatscreen
--- src/components/chatComponents/Chatinput.js
+++ src/components/chatComponents/Chatinput.js
@@ -1,8 +1,29 @@
 import { Button, TextInput } from "@mantine/core"
+import { useForm } from '@mantine/hooks'
+import db from '../firebase/firebaseConfig'
+import { addDoc, collection, Timestamp } from 'firebase/firestore'
+
 function Chatinput(){
-	return <form className="Chatinput">
-            <TextInput className="messageBox" type="text" name="message" autoFocus="true" required="true" placeholder="Message..." maxLength={100} />
-            <Button>Send</Button>
+
+    const form = useForm({
+        initialValues: {
+            user:"",
+            mes: "",
+        }
+    })
+
+    const sendMessage = (v)=>{
+        const time = new Timestamp()
+        const docRef = addDoc(collection(db, 'mainChat'), {
+            sender: v.user,
+            message: v.mes,
+            timeStamp: Timestamp.now()
+        })
+    }
+
+	return <form onSubmit={form.onSubmit((values)=>sendMessage(values))} className="Chatinput">
+             <TextInput {...form.getInputProps('mes')} className="messageBox" type="text" name="message" autoFocus={true} required={true} placeholder="Message..." maxLength={100} />
+            <Button type="submit">Send</Button>
 		   </form>
 }
 export default Chatinput
